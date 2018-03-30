d67d3b21a4c992beec991c14fe5c9725d9cc3017
Jesse Szwedko <jesse.szwedko@gmail.com>
2018-03-30 12:07:49 -0700

    Revert "Add trusted host settings"
    This reverts commit 93d120ab1c7a16d0e48f676ee12236f27f501af4.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -40,19 +40,3 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT']) && $_ENV['PANTHEON_ENVIRONMENT'] == 'li
 } else {
   $config['reroute_email.settings']['enable'] = TRUE;
 }
-
-// alleviate trusted host setting warnings
-// Pantheon isn't susceptible to them, but it still reduces noise
-// https://pantheon.io/docs/settings-php/
-if (defined('PANTHEON_ENVIRONMENT')) {
-  if (in_array($_ENV['PANTHEON_ENVIRONMENT'], array('dev', 'test', 'live'))) {
-    $settings['trusted_host_patterns'][] = "{$_ENV['PANTHEON_ENVIRONMENT']}-{$_ENV['PANTHEON_SITE_NAME']}.getpantheon.io";
-    $settings['trusted_host_patterns'][] = "{$_ENV['PANTHEON_ENVIRONMENT']}-{$_ENV['PANTHEON_SITE_NAME']}.pantheon.io";
-    $settings['trusted_host_patterns'][] = "{$_ENV['PANTHEON_ENVIRONMENT']}-{$_ENV['PANTHEON_SITE_NAME']}.pantheonsite.io";
-    $settings['trusted_host_patterns'][] = "{$_ENV['PANTHEON_ENVIRONMENT']}-{$_ENV['PANTHEON_SITE_NAME']}.panth.io";
-
-    # Replace value with custom domain(s) added in the site Dashboard
-    $settings['trusted_host_patterns'][] = '^.+.yoursite.com$';
-    $settings['trusted_host_patterns'][] = '^yoursite.com$';
-  }
-}
